d2ab4eb3fd64ad4f6d16d6569ae378880b884820
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2024-10-01 17:23:48 +0200

    Set up styles and functions

---
+++ script.js
@@ -0,0 +1,51 @@
+const h1 = document.querySelector('h1');
+const showButton = document.querySelector('.container > button');
+const dialog = document.querySelector('dialog');
+const closeBtn = document.querySelector('.close');
+const submitBtn = dialog.querySelector('button[type="submit"');
+const bookList = document.querySelector('div.books');
+const title = dialog.querySelector('#title');
+const author = dialog.querySelector('#author');
+const pages = dialog.querySelector('#pages');
+
+booksArr = [];
+
+window.addEventListener('load', () => {
+    h1.style.transform = 'translateY(0)';
+    showButton.style.transform = 'translateY(0)';
+})
+
+showButton.addEventListener('click', () => {
+    dialog.showModal();
+    dialog.classList.add('open');
+})
+
+closeBtn.addEventListener('click', () => {
+    dialog.classList.remove('open');
+    setTimeout(() => {
+        dialog.close();
+    }, 500);
+})
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const isRead = document.querySelector('input[name="read"]:checked');
+    const newBook = new Book(title.value, author.value, pages.value, +isRead.value);
+    const bookEntry = document.createElement('ul');
+    bookEntry.classList.add('book');
+    bookEntry.innerHTML = `
+    <li>${newBook.title}</li>
+    <li>${newBook.author}</li>
+    <li>${newBook.pages}</li>
+    <li>${newBook.read}</li>
+    `
+    bookList.appendChild(bookEntry);
+})
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = (read == true);
+}
+
